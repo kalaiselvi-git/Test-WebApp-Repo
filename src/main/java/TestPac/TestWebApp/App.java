9d10d10a07b783c8e87e3a1866ee601cf61c03ef
kalai selvi <kalai.preathy@gmail.com>
2017-05-16 11:23:29 +0530

    commit 1-TestwebApp added to git

---
+++ src/main/java/TestPac/TestWebApp/App.java
@@ -0,0 +1,13 @@
+package TestPac.TestWebApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
